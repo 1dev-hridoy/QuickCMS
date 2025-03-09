c90b148903f2e8a3f1b84e4ec295d8549fd2b9e6
Hridoy <154232641+1dev-hridoy@users.noreply.github.com>
2025-03-09 17:32:48 +0600

    Add logout functionality and update navbar links for settings and logout

--- admin/includes/__navbar__.php
+++ admin/includes/__navbar__.php
@@ -23,10 +23,9 @@ $title = $settings['name'] ?? '';
                             <i class="bi bi-person-circle"></i>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-person me-2"></i>Profile</a></li>
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-gear me-2"></i>Settings</a></li>
+                            <li><a class="dropdown-item" href="./settings.php"><i class="bi bi-gear me-2"></i>Settings</a></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
+                            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
                         </ul>
                     </div>
                 </div>
